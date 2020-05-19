e531a7006207615702899e37934ef348733bede0
Gerrit Alex <admin@gerritalex.de>
2020-05-19 15:07:59 +0200

    fixes #40

--- src/routes/Profile/Graph.js
+++ src/routes/Profile/Graph.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import { ExternalLink } from '../../components';
+import { useProfile } from '../../hooks';
 
 export default function Graph({ children }) {
+  const { name } = useProfile();
+
   return (
     <div className="module-body chart">
       <table className="candyStriped chart">
         <tbody>{children}</tbody>
       </table>
       <div className="moduleOptions">
-        <ExternalLink href="http://last.fm/user/XHS207GA/charts?rangetype=3month&amp;subtype=artists">
+        <ExternalLink href={`http://last.fm/user/${name}/charts?rangetype=3month&amp;subtype=artists`}>
           See more
         </ExternalLink>
       </div>
